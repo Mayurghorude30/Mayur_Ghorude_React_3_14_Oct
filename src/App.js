3f0bc231c7b1dc7b29b7b5e87abedf285875eda3
mayur ghorude <mayurghorude30@gmail.com>
2023-10-14 14:56:20 +0530

    react

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import SignupPage from './SignupPage';
+import LoginPage from './LoginPage';
+import ProfilePage from './ProfilePage';
+import { UserProvider } from './UserContext';
+import "./App.css"
+function App() {
+  return (
+    <UserProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+        </Routes>
+      </BrowserRouter>
+    </UserProvider>
+  );
+}
+
+export default App;
